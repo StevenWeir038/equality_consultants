b744aebf918dec39f90d718726f6e0fb83c6d9be
StevenWeir038 <weir_steven@btinternet.com>
2022-09-07 14:21:03 +0000

    Create variables for contact form elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-console.log("Test JS link is working")
+/* jshint esversion: 8 */
+
+/* Variables for contact form buttons */
+const contactForm =  document.getElementById("contact-form");
+const contactFormName =  document.getElementById("contact-form-name");
+const contactFormEmail =  document.getElementById("contact-form-email");
+const contactFormMessage =  document.getElementById("contact-form-message");
+const contactFormButton =  document.getElementById("contact-form-button");
