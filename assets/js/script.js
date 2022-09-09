7898ba732850033c6b8501205395b0c5b6167ec0
Francisca-Heii <franciscahheii@gmail.com>
2022-09-09 19:59:25 +0100

    Added favicon, logo images,updated toggle navbar on small devices and added related styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
+// jshint esversion: 6
+/*jshint sub:true*/
+//Script for active navigation menu
+$(document).ready(function () {
+    $("ul.navbar-nav > li > a").click(
+      function (e) {
+        $("ul.navbar-nav > li").removeClass("active");
+        $("ul.navbar-nav > li > a").css("color", "");
+        $("ul.navbar-nav > li > a").css("background", "");  
+        $(this).addClass("active");
+        $(this).css("color", "green");
+        $(this).css("background", "white");
+    });
+});
+
+
+
 console.log("Test JS link is working")
+
